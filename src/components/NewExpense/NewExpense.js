1e3a4fbbc00169e9e8e4c4473da0be7b6b1b26bd
dex-devries <dexter.m.devries@gmail.com>
2023-03-06 13:50:56 -0700

    NewExpense.js commented

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -2,22 +2,27 @@ import "./NewExpense.css";
 import ExpenseForm from "./ExpenseForm";
 import React, { useState } from "react";
 
+// NewExpense component expects props.onAddExpense from App.js
 const NewExpense = (props) => {
+  // state for new expense menu open (true) or closed (false)
   const [menuOpen, setMenuOpen] = useState(false);
 
-  // Handlers for toggle and save expense
+  // Handler for toggle menu open/closed (pass to ExpenseForm component)
   const toggleMenuHandler = (open) => {
     open ? setMenuOpen(true) : setMenuOpen(false);
   }
+  // Handler for saved expenses in ExpenseForm (pass to ExpenseForm component)
   const saveExpenseHandler = (enteredData) => {
     const expenseData = {
       ...enteredData,
+      // Random ID needs work - could duplicate
       id: Math.floor((Math.random() * 10) ^ 10).toString(),
     };
+    // pass added expense to App
     props.onAddExpense(expenseData);
   };
 
-   // Variables to store JSX return
+   // Variables to store JSX returns
    const menuOpenJSX = (
     <div className="new-expense">
       <ExpenseForm onSave={saveExpenseHandler} onCancel={toggleMenuHandler}/>
@@ -30,7 +35,7 @@ const NewExpense = (props) => {
       }}>Add Expense</button>
     </div>
   )
-
+  // conditional for menu open
   if (menuOpen) {
     return menuOpenJSX;
   }
