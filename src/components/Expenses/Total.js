f829316553c0a9400d04c210c1f1069a299bd6ba
dex-devries <dexter.m.devries@gmail.com>
2023-03-06 15:47:54 -0700

    Fixed decimal points for dollar amounts toFixed(2)

--- src/components/Expenses/Total.js
+++ src/components/Expenses/Total.js
@@ -2,7 +2,7 @@ const Total = (props) => {
   return (
     <div className="expenses-filter">
       <div className="expenses-filter__control">
-        <label>Total for selection: ${props.total}</label>
+        <label>Total for selection: ${props.total.toFixed(2)}</label>
       </div>
     </div>
   );
